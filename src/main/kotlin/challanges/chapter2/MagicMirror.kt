e17d8a607c2fc799ef3c41571627fa6038bb1f9b
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2022-01-09 15:56:00 -0500

    Chapter 2. Variables, Constants, and Types. Challenge: Magic Mirror.

---
+++ src/main/kotlin/challanges/chapter2/MagicMirror.kt
@@ -0,0 +1,16 @@
+package challanges.chapter2
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    val heroName = "Madrigal"
+    println(heroName)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+
+    println(heroName.reversed())
+}
