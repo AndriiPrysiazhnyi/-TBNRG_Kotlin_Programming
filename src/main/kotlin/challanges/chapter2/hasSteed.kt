55d998010b69db54f5059f4df7d6ae46fdfd8f07
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2022-01-09 15:32:16 -0500

    Chapter 2. Variables, Constants, and Types. Challenge: hasSteed.

---
+++ src/main/kotlin/challanges/chapter2/hasSteed.kt
@@ -0,0 +1,18 @@
+package challanges.chapter2
+
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+
+    var hasSteed = "Madrigal do not have a Dragon or Minotaur to ride yet."
+    println(hasSteed)
+}
