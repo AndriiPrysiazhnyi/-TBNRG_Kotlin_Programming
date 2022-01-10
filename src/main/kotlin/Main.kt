f52fb79807e87cd339dbf2228e1abd261e0853a1
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2022-01-10 09:51:19 -0500

    Chapter 4. Functions. Single-Expression Functions.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -25,25 +25,22 @@ private fun readBountyBoard() {
 
 private fun obtainQuest(
     playerLevel: Int,
-    playerClass: String= "paladin",
+    playerClass: String = "paladin",
     hasBefriendedBarbarians: Boolean = true,
     hasAngeredBarbarians: Boolean = false
-): String {
-    val quest: String = when (playerLevel) {
-        1 -> "Meet Mr. Bubbles in the land of soft things."
-        in 2..5 -> {
-            val canTalkToBarbarians = !hasAngeredBarbarians &&
-                    (hasBefriendedBarbarians || playerClass == "barbarian")
-            if (canTalkToBarbarians) {
-                "Convince the barbarians to call off their invasion."
-            } else {
-                "Save the town from barbarian invasions."
-            }
+):String = when (playerLevel) {
+    1 -> "Meet Mr. Bubbles in the land of soft things."
+    in 2..5 -> {
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from barbarian invasions."
         }
-        6 -> "Locate the enchanted sword."
-        7 -> "Recover the long-lost artifact of creation."
-        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-        else -> "There are no quests right now."
     }
-    return quest
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+    else -> "There are no quests right now."
 }
