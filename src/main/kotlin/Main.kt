886acf29b4edba0e8f1d979b9849c1e2fb19a3eb
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2022-01-11 20:18:47 -0500

    Chapter 7. Null Safety and Exceptions. Throwing an exception.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,25 +31,29 @@ private fun readBountyBoard() {
     println(message)
 }
 
-
 private fun obtainQuest(
     playerLevel: Int,
     playerClass: String = "paladin",
     hasBefriendedBarbarians: Boolean = true,
     hasAngeredBarbarians: Boolean = false
-): String? = when (playerLevel) {
-    1 -> "Meet Mr. Bubbles in the land of soft things."
-    in 2..5 -> {
-        val canTalkToBarbarians = !hasAngeredBarbarians &&
-                (hasBefriendedBarbarians || playerClass == "barbarian")
-        if (canTalkToBarbarians) {
-            "Convince the barbarians to call off their invasion."
-        } else {
-            "Save the town from barbarian invasions."
-        }
+): String? {
+    if (playerLevel <= 0){
+        throw IllegalArgumentException("The player's level must be at least 1.")
     }
-    6 -> "Locate the enchanted sword."
-    7 -> "Recover the long-lost artifact of creation."
-    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-    else -> null
+    return when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from barbarian invasions."
+            }
+        }
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> null
+    }
 }
