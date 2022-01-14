f50f4153249895e1e2d1d2543c9e58f5db3c1e0e
AndriiPrysiazhnyi <prysiazhnyi.andrii@gmail.com>
2022-01-14 11:38:05 -0500

    Chapter 9. Lists and Sets. Lists.

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,6 +1,9 @@
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
-fun visitTavern(){
+fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
 
+    val patrons: List<String> = listOf("Eli", "Mordoc", "Sophie")
+    println(patrons)
 }
